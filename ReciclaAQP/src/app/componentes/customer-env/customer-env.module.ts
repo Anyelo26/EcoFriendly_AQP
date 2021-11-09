a8c28f2cede37f8986e8e845fd992359856ee2ac
Solange2724 <55797838+Solange2724@users.noreply.github.com>
2021-11-09 16:05:57 -0500

    Modulo Información - Cliente A1

--- ReciclaAQP/src/app/componentes/customer-env/customer-env.module.ts
+++ ReciclaAQP/src/app/componentes/customer-env/customer-env.module.ts
@@ -11,6 +11,23 @@ import { CollectionCenterComponent } from './dynamic/collection-center/collectio
 import { InformationComponent } from './dynamic/information/information.component';
 import { FootprintComponent } from './dynamic/footprint/footprint.component';
 
+//Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDividerModule } from '@angular/material/divider';
+import { MatGridListModule} from '@angular/material/grid-list';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule} from '@angular/material/input';
+import { MatRadioModule} from '@angular/material/radio';
+import { MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {MatCardModule} from '@angular/material/card';
+
 
 @NgModule({
   declarations: [ 
@@ -26,6 +43,21 @@ import { FootprintComponent } from './dynamic/footprint/footprint.component';
   imports: [
     CommonModule,
     CustomerEnvRoutingModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatCardModule,
   ]
 })
 export class CustomerEnvModule { }
